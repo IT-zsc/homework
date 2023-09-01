7e7575789d6d4c5ae50212fcc7fb2fc07957531d
zongzonga <1279724257@qq.com>
2023-09-01 22:04:57 +0800

    Initial commit

---
+++ src/main/java/abstract20230901/Test.java
@@ -0,0 +1,16 @@
+package abstract20230901;
+
+/**
+ * ClassName:Test
+ * Package:abstract20230901
+ * Description:
+ *
+ * @Author: 宗树成
+ * @Create: 2023/9/1-21:57
+ * @version: v1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello update");
+    }
+}
