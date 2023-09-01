e80d95324eb787c9c2539ea5767409ff75a0e582
zongzonga <1279724257@qq.com>
2023-09-01 22:27:20 +0800

    Initial commit

---
+++ src/main/java/abstract20230901/Test2.java
@@ -0,0 +1,16 @@
+package abstract20230901;
+
+/**
+ * ClassName:Test2
+ * Package:abstract20230901
+ * Description:
+ *
+ * @Author: 宗树成
+ * @Create: 2023/9/1-22:21
+ * @version: v1.0
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("zzz");
+    }
+}
---
+++ src/main/java/abstract20230901/tets3.java
@@ -0,0 +1,13 @@
+package abstract20230901;
+
+/**
+ * ClassName:tets3
+ * Package:abstract20230901
+ * Description:
+ *
+ * @Author: 宗树成
+ * @Create: 2023/9/1-22:26
+ * @version: v1.0
+ */
+public class tets3 {
+}
